5213bee75ccca100e10a7fefbb30ae802a94034a
Sashko Potapov <35269245+sashkopotapov@users.noreply.github.com>
2022-10-05 11:59:34 +0300

    Fixed internal protection level of asyncMap (#6)
    Co-authored-by: Anthony Drendel <me@anthonydrendel.com>
    Co-authored-by: Anthony Drendel <atdrendel@users.noreply.github.com>

---
+++ Tests/AsyncExtensionsTests/SequenceExtensionsTests.swift
@@ -0,0 +1,60 @@
+@testable import AsyncExtensions
+import AsyncTestExtensions
+import Synchronized
+import XCTest
+
+final class MapFunctionsTests: XCTestCase {
+    func testAsyncMapRunsTasksSerially() async throws {
+        struct TestState {
+            var concurrentTestCount = 0 {
+                didSet {
+                    if concurrentTestCount > 1 {
+                        didRunTestsConcurrently = true
+                    }
+                }
+            }
+
+            var didRunTestsConcurrently = false
+        }
+
+        let state = Locked(TestState())
+
+        let square: (Int) async throws -> Int = {
+            state.access { $0.concurrentTestCount += 1 }
+            defer { state.access { $0.concurrentTestCount -= 1 } }
+            try await Task.sleep(nanoseconds: 10_000_000)
+            return $0 * $0
+        }
+
+        let squares = try await [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].asyncMap(square)
+        XCTAssertEqual([1, 4, 9, 16, 25, 36, 49, 64, 81, 100], squares)
+        XCTAssertFalse(state.access { $0.didRunTestsConcurrently })
+    }
+
+    func testConcurrentMapRunsTasksConcurrently() async throws {
+        struct TestState {
+            var concurrentTestCount = 0 {
+                didSet {
+                    if concurrentTestCount > 1 {
+                        didRunTestsConcurrently = true
+                    }
+                }
+            }
+
+            var didRunTestsConcurrently = false
+        }
+
+        let state = Locked(TestState())
+
+        let square: (Int) async throws -> Int = {
+            state.access { $0.concurrentTestCount += 1 }
+            defer { state.access { $0.concurrentTestCount -= 1 } }
+            try await Task.sleep(nanoseconds: 10_000_000)
+            return $0 * $0
+        }
+
+        let squares = try await [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].concurrentMap(square)
+        XCTAssertEqual([1, 4, 9, 16, 25, 36, 49, 64, 81, 100], squares)
+        XCTAssertTrue(state.access { $0.didRunTestsConcurrently })
+    }
+}
