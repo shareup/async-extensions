633a9b134f6cff2a094cedffb940d811a6568482
Anthony Drendel <me@anthonydrendel.com>
2022-12-06 13:36:08 +0100

    Support Future cancellation

--- Sources/AsyncExtensions/Future.swift
+++ Sources/AsyncExtensions/Future.swift
@@ -1,71 +1,103 @@
 import Foundation
 import Synchronized
 
-public final class Future<T: Sendable>: @unchecked Sendable {
-    private let result = Locked<Result<T, Error>?>(nil)
-    private let continuations = Locked<[UnsafeContinuation<T, Error>]>([])
+public final class Future<T: Sendable>: @unchecked
+Sendable {
+    fileprivate typealias R = Result<T, Error>
+    fileprivate typealias C = UnsafeContinuation<T, Error>
+    fileprivate typealias Cs = [String: C]
+
+    private let state = Locked(State())
 
     public init() {}
 
-    public init(_ block: @escaping () async throws -> T) {
-        Task {
-            do {
-                let value = try await block()
-                self.resolve(value)
-            } catch {
-                self.fail(error)
-            }
-        }
-    }
-
-    public init(_ block: (@escaping (Result<T, Error>) -> Void) -> Void) {
-        block { result in
-            switch result {
-            case let .success(value):
-                self.resolve(value)
-
-            case let .failure(error):
-                self.fail(error)
-            }
-        }
-    }
-
     public var value: T { get async throws {
-        try await withUnsafeThrowingContinuation { cont in
-            if let result = result.access({ $0 }) {
-                cont.resume(with: result)
-            } else {
-                continuations.access { $0.append(cont) }
-                // If the result was set while adding our continuation to
-                // the array of continuations, we need to resume it.
-                resumeContinuations()
+        let id = UUID().uuidString
+
+        return try await withTaskCancellationHandler(
+            operation: {
+                try await withUnsafeThrowingContinuation { cont in
+                    let resultOrIsCancelled: (R?, Bool) =
+                        state.access { state in
+                            if state.cancelled.contains(id) {
+                                return (nil, true)
+                            } else if let result = state.result {
+                                return (result, false)
+                            } else {
+                                state.continuations[id] = cont
+                                return (nil, false)
+                            }
+                        }
+
+                    if let result = resultOrIsCancelled.0 {
+                        cont.resume(with: result)
+                    } else if resultOrIsCancelled.1 {
+                        cont.resume(throwing: CancellationError())
+                    }
+
+                    resumeContinuations()
+                }
+            },
+            onCancel: {
+                let cont = state.access { state in
+                    state.cancelled.insert(id)
+                    return state
+                        .continuations
+                        .removeValue(forKey: id)
+                }
+                cont?.resume(throwing: CancellationError())
             }
-        }
+        )
     }}
 
     public func resolve(_ value: T) {
-        result.access { result in
-            guard result == nil else { return }
-            result = .success(value)
+        state.access { state in
+            guard state.result == nil else { return }
+            state.result = .success(value)
         }
         resumeContinuations()
     }
 
     public func fail(_ error: Error) {
-        result.access { result in
-            guard result == nil else { return }
-            result = .failure(error)
+        state.access { state in
+            guard state.result == nil else { return }
+            state.result = .failure(error)
         }
         resumeContinuations()
     }
 
     private func resumeContinuations() {
-        guard let result = result.access({ $0 }) else { return }
-        let continuations = continuations.access { continuations in
-            let conts = continuations
-            continuations.removeAll()
-            return conts
+        let rAndCs: (R, Cs)? = state.access { state in
+            guard let result = state.result else { return nil }
+            let conts = state.continuations
+            state.continuations.removeAll()
+            return (result, conts)
         }
-        continuations.forEach { $0.resume(with: result) }
+
+        guard let (result, continuations) = rAndCs else { return }
+        continuations.forEach { $0.value.resume(with: result) }
+    }
+}
+
+public extension Future {
+    var result: Result<T, Error> { get async {
+        do {
+            let value = try await value
+            return .success(value)
+        } catch {
+            return .failure(error)
+        }
+    }}
+}
+
+public extension Future where T == Void {
+    func resolve() { resolve(()) }
+}
+
+private extension Future {
+    struct State {
+        var result: R?
+        var continuations: Cs = [:]
+        var cancelled: Set<String> = []
     }
 }
