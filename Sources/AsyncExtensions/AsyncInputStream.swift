83f4607fb5d4c777f191c0a2feefc5e49b53e0a8
Anthony Drendel <me@anthonydrendel.com>
2023-01-11 23:45:52 +0100

    Refactor AsyncInputStream

--- Sources/AsyncExtensions/AsyncInputStream.swift
+++ Sources/AsyncExtensions/AsyncInputStream.swift
@@ -5,8 +5,11 @@ public final class AsyncInputStream {
     // Modeled after https://github.com/apple/swift-async-algorithms/blob/a973b06d06f2be355c562ec3ce031373514b03f5/Sources/AsyncAlgorithms/AsyncBufferedByteIterator.swift#L34
     private static let defaultMaxChunkSize: Int = 16 * 1024
 
-    private let stream: InputStream
+    private static let defaultBufferSize: Int = 512 * 1024
+
     private let maxChunkSize: Int
+
+    private var state: State
 
     public convenience init(url: URL) throws {
         try self.init(url: url, maxChunkSize: Self.defaultMaxChunkSize)
@@ -17,30 +20,35 @@ public final class AsyncInputStream {
     }
 
     internal init(url: URL, maxChunkSize: Int) throws {
+        self.maxChunkSize = maxChunkSize
+
         guard let stream = InputStream(url: url)
         else { throw AsyncInputStreamError.couldNotOpenURL(url) }
 
-        self.stream = stream
-        self.maxChunkSize = maxChunkSize
+        let bufferSize = Self.defaultBufferSize
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        buffer.initialize(repeating: 0, count: bufferSize)
 
-        self.stream.open()
+        state = .open(stream, buffer, bufferSize: bufferSize)
+
+        stream.open()
     }
 
     internal init(data: Data, maxChunkSize: Int) {
-        stream = InputStream(data: data)
         self.maxChunkSize = maxChunkSize
+
+        let stream = InputStream(data: data)
+        let bufferSize = Self.defaultBufferSize
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        buffer.initialize(repeating: 0, count: bufferSize)
+
+        state = .open(stream, buffer, bufferSize: bufferSize)
 
         stream.open()
     }
 
     deinit {
-        switch stream.streamStatus {
-        case .closed:
-            break
-
-        default:
-            stream.close()
-        }
+        state.close()
     }
 
     public func read<I: FixedWidthInteger>() async throws -> I {
@@ -59,66 +67,149 @@ public final class AsyncInputStream {
     public func read(maxLength len: Int) async throws -> [UInt8]? {
         guard len > 0 else { return nil }
 
-        if case .closed = stream.streamStatus {
-            throw AsyncInputStreamError.closed
+        do {
+            return try await doRead(maxLength: len)
+        } catch {
+            state.fail(error)
+            throw error
         }
-
-        let task: Task<[UInt8]?, Error> = Task {
-            var buffer = [UInt8](repeating: 0, count: len)
-            var totalBytesRead = 0
-            var shouldStop = false
-
-            repeat {
-                guard !Task.isCancelled else {
-                    throw CancellationError()
-                }
-
-                let chunkSize = min(len - totalBytesRead, self.maxChunkSize)
-
-                let bytesRead = await self._read(
-                    chunkSize: chunkSize,
-                    offset: totalBytesRead,
-                    into: &buffer
-                )
-
-                switch bytesRead {
-                case -1:
-                    self.stream.close()
-                    throw self.stream.streamError ?? AsyncInputStreamError.unknown
-
-                case 0:
-                    shouldStop = true
-
-                default:
-                    totalBytesRead += bytesRead
-
-                    if bytesRead < chunkSize {
-                        shouldStop = true
-                    }
-                }
-
-            } while !shouldStop && len > totalBytesRead
-
-            if totalBytesRead > 0 {
-                buffer.removeLast(len - totalBytesRead)
-                return buffer
-            } else {
-                stream.close()
-                return nil
-            }
-        }
-
-        return try await task.value
     }
 
-    private func _read(
-        chunkSize: Int,
-        offset: Int,
-        into buffer: UnsafeMutablePointer<UInt8>
-    ) async -> Int {
-        await withUnsafeContinuation { (cont: UnsafeContinuation<Int, Never>) in
-            let bytesRead = stream.read(buffer.advanced(by: offset), maxLength: chunkSize)
-            cont.resume(returning: bytesRead)
+    private func doRead(maxLength len: Int) async throws -> [UInt8]? {
+        try Task.checkCancellation()
+
+        guard try state.checkOpen() else { return nil }
+
+        state.resizeBuffer(to: len)
+
+        var totalBytesRead = 0
+        var shouldStop = false
+
+        repeat {
+            try Task.checkCancellation()
+
+            let chunkSize = min(len - totalBytesRead, maxChunkSize)
+
+            let bytesRead = try state.read(chunkSize, offset: totalBytesRead)
+            precondition(bytesRead >= 0)
+
+            shouldStop = bytesRead == 0 || bytesRead < chunkSize
+
+            totalBytesRead += bytesRead
+
+            await Task.yield()
+        } while !shouldStop && len > totalBytesRead
+
+        if totalBytesRead > 0 {
+            return try state.copyBytesAndReset(totalBytesRead)
+        } else {
+            state.close()
+            return nil
+        }
+    }
+}
+
+private enum State {
+    case closed
+    case failed(Error)
+    case open(InputStream, UnsafeMutablePointer<UInt8>, bufferSize: Int)
+
+    func checkOpen() throws -> Bool {
+        switch self {
+        case .closed:
+            return false
+
+        case let .failed(error):
+            throw error
+
+        case .open:
+            return true
+        }
+    }
+
+    mutating func close() {
+        switch self {
+        case .closed, .failed:
+            break
+
+        case let .open(stream, buffer, bufferSize):
+            stream.close()
+            buffer.deinitialize(count: bufferSize)
+            buffer.deallocate()
+            self = .closed
+        }
+    }
+
+    mutating func fail(_ error: Error) {
+        switch self {
+        case .closed, .failed:
+            break
+
+        case let .open(stream, buffer, bufferSize):
+            stream.close()
+            buffer.deinitialize(count: bufferSize)
+            buffer.deallocate()
+            self = .failed(error)
+        }
+    }
+
+    func read(_ length: Int, offset: Int) throws -> Int {
+        switch self {
+        case .closed:
+            throw AsyncInputStreamError.closed
+
+        case let .failed(error):
+            throw error
+
+        case let .open(stream, buffer, bufferSize):
+            precondition(offset + length <= bufferSize)
+            let bytesRead = stream.read(buffer.advanced(by: offset), maxLength: length)
+            guard bytesRead >= 0
+            else { throw stream.streamError ?? POSIXError(.EIO) }
+            return bytesRead
+        }
+    }
+
+    func copyBytesAndReset(_ size: Int) throws -> [UInt8] {
+        switch self {
+        case .closed:
+            throw AsyncInputStreamError.closed
+
+        case let .failed(error):
+            throw error
+
+        case let .open(_, buffer, bufferSize):
+            // This does not copy the bytes.
+            let validBuffer = UnsafeMutableBufferPointer(
+                start: buffer,
+                count: size
+            )
+
+            // This copies the bytes into the array.
+            let bytes = Array(validBuffer)
+
+            buffer.assign(repeating: 0, count: bufferSize)
+
+            return bytes
+        }
+    }
+
+    mutating func resizeBuffer(to newBufferSize: Int) {
+        switch self {
+        case .closed, .failed:
+            break
+
+        case let .open(stream, buffer, bufferSize):
+            guard newBufferSize > bufferSize else { return }
+
+            buffer.deinitialize(count: bufferSize)
+            buffer.deallocate()
+
+            let newBuffer = UnsafeMutablePointer<UInt8>
+                .allocate(capacity: newBufferSize)
+            newBuffer.initialize(repeating: 0, count: newBufferSize)
+
+            self = .open(stream, newBuffer, bufferSize: newBufferSize)
         }
     }
 }
@@ -127,7 +218,6 @@ public enum AsyncInputStreamError: Error, Equatable {
     case closed
     case couldNotOpenURL(URL)
     case couldNotReadFixedWidthInteger(Int)
-    case invalidBufferPointer
     case unknown
 }
 
